e59b20f616495728086a5f34403827c584fa780f
JECHUser <66657576+JECHUser@users.noreply.github.com>
2022-02-23 04:26:50 -0600

    Datasheets y Correcciones

--- workspace_v10/ball_n_beam_cssproject/main.c
+++ workspace_v10/ball_n_beam_cssproject/main.c
@@ -4,8 +4,226 @@
  * main.c
  */
 
+
+
+
 #include <stdint.h>
-int main(void)
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/adc.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/ssi.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "sensorlib/i2cm_drv.h"
+#include "sensorlib/i2cm_drv.c"
+#include "sensorlib/hw_mpu6050.h"
+#include "sensorlib/mpu6050.h"
+#include "sensorlib/mpu6050.c"
+
+//*****************************************************************************
+// Definiciones para configuración del SPI
+//*****************************************************************************
+#define NUM_SPI_DATA   1    // Número de palabras que se envían cada vez
+#define SPI_FREC 4000000    // Frecuencia para el reloj del SPI
+#define SPI_ANCHO     16    // Número de bits que se envían cada vez, entre 4 y 16
+
+//*****************************************************************************
+// Variables para Comunicación SPI
+//*****************************************************************************
+uint16_t dato;
+uint16_t data_out;
+uint32_t pui32DataTx[NUM_SPI_DATA];
+
+//*****************************************************************************
+// Variables para Controlador PID
+//*****************************************************************************
+uint16_t temp1 = 0, temp2 = 0;
+uint32_t pui32ADC0Value[2];     // Spaces for processed data
+float accelerometer_reading[3], gyro_reading[3];
+
+void
+Timer0IntHandler(void)
 {
-	return 0;
+
+    // Clear the timer interrupt.
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Trigger the ADC conversion.
+    ADCProcessorTrigger(ADC0_BASE, 2);
+
+    // Wait for conversion to be completed.
+    while(!ADCIntStatus(ADC0_BASE, 2, false))
+    {
+    }
+
+    // Clear the ADC interrupt flag.
+    ADCIntClear(ADC0_BASE, 2);
+
+    // Data Acquisition
+    ADCSequenceDataGet(ADC0_BASE, 2, pui32ADC0Value);
+
+    temp1 = (uint32_t)pui32ADC0Value[0];
+    temp2 = (uint32_t)pui32ADC0Value[1];
+
+}
+
+void
+InitConsole(void)
+{
+    // Enable GPIO port A which is used for UART0 pins.
+    // TODO: change this to whichever GPIO port you are using.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    // Configure the pin muxing for UART0 functions on port A0 and A1.
+    // This step is not necessary if your part does not support pin muxing.
+    // TODO: change this to select the port/pin you are using.
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+
+    // Enable UART0 so that we can configure the clock.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    // Use the internal 16MHz oscillator as the UART clock source.
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    // Select the alternate (UART) function for these pins.
+    // TODO: change this to select the port/pin you are using.
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(0, 115200, 16000000);
+}
+
+int
+main(void)
+{
+    uint32_t pui32residual[NUM_SPI_DATA];
+    uint16_t freq_muestreo = 1000;   // En Hz
+
+    // Set the clocking to run at 80 MHz (200 MHz / 2.5) using the PLL.  When
+    // using the ADC, you must either use the PLL or supply a 16 MHz clock source.
+    // TODO: The SYSCTL_XTAL_ value must be changed to match the value of the
+    // crystal on your board.
+    //SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+    //                 SYSCTL_XTAL_16MHZ); // 20 MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ); // 80 MHz
+
+    // Set up the serial console to use for displaying messages.  This is
+    // just for this example program and is not needed for ADC operation.
+    InitConsole();
+
+    //*****************************************************************************
+    // Configuración SPI
+    //*****************************************************************************
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
+    GPIOPinConfigure(GPIO_PA3_SSI0FSS);
+    GPIOPinConfigure(GPIO_PA4_SSI0RX);
+    GPIOPinConfigure(GPIO_PA5_SSI0TX);
+
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3 |
+                   GPIO_PIN_2);
+
+    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0,
+                           SSI_MODE_MASTER, SPI_FREC, SPI_ANCHO);
+
+    SSIEnable(SSI0_BASE);
+
+    while(SSIDataGetNonBlocking(SSI0_BASE, &pui32residual[0]))
+        {
+
+        }
+    //*****************************************************************************
+    // Configuración Canales ADC
+    //*****************************************************************************
+
+    // The ADC0 peripheral must be enabled for use.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+
+    // For this example ADC0 is used with AIN0 and AIN1.
+    // The actual port and pins used may be different on your part, consult
+    // the data sheet for more information.  GPIO port E needs to be enabled
+    // so these pins can be used.
+    // TODO: change this to whichever GPIO port you are using.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+    // Select the analog ADC function for these pins.
+    // Consult the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using.
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);  // Configura el pin PE3
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);  // Configura el pin PE2
+
+    // Se configura la secuencia 2, que permitiría hasta cuatro muestras (aunque
+    // se usarán dos).
+    ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR, 0);
+
+    // Step 0 en la secuencia 2: Canal 0 (ADC_CTL_CH0) en modo single-ended (por defecto).
+    ADCSequenceStepConfigure(ADC0_BASE, 2, 0, ADC_CTL_CH0);
+
+    // Step 1 en la secuencia 2: Canal 1 (ADC_CTL_CH1) en modo single-ended (por defecto),
+    // y configura la bandera de interrupción (ADC_CTL_IE) para "setearse"
+    // cuando se tenga esta muestra. También se indica que esta es la última
+    // conversión en la secuencia 2 (ADC_CTL_END).
+    // Para más detalles del módulo ADC, consultar el datasheet.
+    ADCSequenceStepConfigure(ADC0_BASE, 2, 1, ADC_CTL_CH1 | ADC_CTL_IE | ADC_CTL_END);
+
+    // Since sample sequence 2 is now configured, it must be enabled.
+    ADCSequenceEnable(ADC0_BASE, 2);  // Notar el cambio de "secuencia".
+
+    // Clear the interrupt status flag.  This is done to make sure the
+    // interrupt flag is cleared before we sample.
+    ADCIntClear(ADC0_BASE, 2);  // Notar el cambio de "secuencia".
+
+    //*****************************************************************************
+    // Configuración Timer0
+    //*****************************************************************************
+
+    // Enable the peripherals used by this example.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+    // Enable processor interrupts.
+    IntMasterEnable();
+
+    // Configure the two 32-bit periodic timers.
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+
+    // El tercer argumento determina la frecuencia. El reloj se puede obtener
+    // con SysCtlClockGet().
+    // La frecuencia está dada por SysCtlClockGet()/(valor del 3er argumento).
+    // Ejemplos: Si se pone SysCtlClockGet(), la frecuencia será de 1 Hz.
+    //           Si se pone SysCtlClockGet()/1000, la frecuencia será de 1 kHz
+    //TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet());
+    TimerLoadSet(TIMER0_BASE, TIMER_A, (uint32_t)(SysCtlClockGet()/freq_muestreo));
+
+    // Setup the interrupt for the timer timeout.
+    IntEnable(INT_TIMER0A);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Enable the timers.
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    //*****************************************************************************
+    // Configuración mpu6050
+    //*****************************************************************************
+
+
+    // Las conversiones se hacen al darse la interrupción del timer, para que
+    // el muestreo sea preciso. Luego de las configuraciones, el programa se
+    // queda en un ciclo infinito haciendo nada.
+    while(1)
+    {
+    }
 }
